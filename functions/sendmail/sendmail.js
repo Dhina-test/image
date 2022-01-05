bafec17b575ff9284275b029ca02f911149a1fe3
Surendran S <surendrans@spritle.com>
2022-01-05 20:23:56 +0530

    New attrs

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -8,14 +8,17 @@ const {
 exports.handler = async function (event, context, callback) {
   console.log('-------')
   console.log(event.body)
-  const { message, senderEmail, senderName } = JSON.parse(event.body);
+  JSON.parse(event.body);
+  
+  const { fname, company_name, email_address, phone_number, comment  } = JSON.parse(event.body);
   client.setApiKey(SENDGRID_API_KEY);
 
+  message
   const data = {
     to: SENDGRID_TO_EMAIL,
     from: SENDGRID_FROM_EMAIL,
-    subject: `New message from ${senderName} (${senderEmail})`,
-    html: message,
+    subject: `New message from ${fname} (${email_address})`,
+    html: `Name : ${fname} \nCompany Name: ${company_name} \nEmail: ${email_address} \nPhone no: ${phone_number} \nMessage: ${comment}`,
   };
 
   try {
