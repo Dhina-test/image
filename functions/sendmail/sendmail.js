2584ec06ecef54becc35edf1edc3595174722b97
Surendran S <surendrans@spritle.com>
2022-01-06 13:03:31 +0530

    undefined variable

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -13,7 +13,6 @@ exports.handler = async function (event, context, callback) {
   const { Name, Company_Name, Email, Phone_Number, comment  } = JSON.parse(event.body);
   client.setApiKey(SENDGRID_API_KEY);
 
-  message
   const data = {
     to: SENDGRID_TO_EMAIL,
     from: SENDGRID_FROM_EMAIL,
